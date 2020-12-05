222868955a2027973ec97ed272d20479f6f217e8
Hoàng Lương Trịnh <trinhhoangluong01@gmail.com>
2020-12-05 14:24:45 +0700

    add project

---
+++ product/views.py
@@ -0,0 +1,13 @@
+from product.models import Product
+from product.serializers import ProductSerializer
+from rest_framework import generics
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
