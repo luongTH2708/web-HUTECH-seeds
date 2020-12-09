a063f271d846b32227838a65209682712f622bfa
Hoàng Lương Trịnh <trinhhoangluong01@gmail.com>
2020-12-09 14:58:38 +0700

    add database config

--- web_cps_django/web_cps_django/settings.py
+++ web_cps_django/web_cps_django/settings.py
@@ -75,8 +75,19 @@ WSGI_APPLICATION = 'web_cps_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'cps_webserver',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Hoangluong@123',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
